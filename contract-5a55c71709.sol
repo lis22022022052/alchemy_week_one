5d28a42e144110bbf2892dbf8b40b07b8fd2dbbd
Andrii.Baklanov <andrii.baklanov@anvileight.com>
2022-06-30 20:40:05 +0300

    end of the lesson

--- contract-5a55c71709.sol
+++ contract-5a55c71709.sol
@@ -4,18 +4,19 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
 
-contract Andy is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract Andy is ERC721, ERC721Enumerable, ERC721URIStorage {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    uint256 MAX_SUPPLY = 10000;
 
     constructor() ERC721("Andy", "LIS") {}
 
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId <= MAX_SUPPLY, "Sorry all NFTs have been minted.");
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
